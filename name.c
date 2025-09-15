349b540279eaebcab77989b3880f2a71db053c64
Dimon4iklimon <147753132+Dimon4iklimon@users.noreply.github.com>
2025-09-15 12:57:57 +0300

    Update name.c
    1.1

--- name.c
+++ name.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 
+/*
+* @brief Точка входа в программу 
+* @return возвращает 0 если программа выполена корректно, иначе 1
+*/
+
+double getB(const double x, const double y, const double z);
+
+double getA(const double x, const double y, const double z);
+
 int main()
 {
-  printf("Hello World");
+	const double x = 3.1;
+	const double y = 2.3;
+	const double z = -1.25;
 
-  return 0;
-  
+	return 0;
+
+}
+
+double getB(const double x, const double y, const double z)
+{
+	double a = getA(x, y, z);
+	return sin(a) + exp(x + y) + z;
+
+}
+
+double getA(const double x, const double y, const double z)
+{
+
 }
